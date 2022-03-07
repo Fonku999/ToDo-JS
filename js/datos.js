0188df75b3da8bdd8d1773be06f8f6feee26c4fe
Pascual <pascual.contrerasfoncubierta@gmail.com>
2022-03-07 18:36:30 +0100

    Commit

---
+++ js/datos.js
@@ -0,0 +1,20 @@
+let listaRutinas = new Array(
+    {
+        idRutina: 1,
+        titulo: 'Sacar al Perro',
+        prioridad: 'diario'
+    },
+
+    {
+        idRutina: 2,
+        titulo: 'Deportes',
+        prioridad: 'semanal'
+    },
+
+    {
+        idRutina: 3,
+        titulo: 'Podar el Jardin',
+        prioridad: 'mensual'
+    },
+
+)
